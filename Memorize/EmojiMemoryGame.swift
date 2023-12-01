48f9d87718eff7fbcf7fe9946f5e1e70f22900dd
ijkVector <johnsmith28122000@gmail.com>
2023-12-01 15:45:58 +0300

    Add: ViewModel - component of MVVM

---
+++ Memorize/EmojiMemoryGame.swift
@@ -0,0 +1,16 @@
+//
+//  EmojiMemoryGame.swift
+//  Memorize
+//
+//  Created by Иван Дроботов on 01.12.2023.
+//
+
+import SwiftUI
+
+class EmojiMemoryGame {
+    var model: MemorizeGame<String>
+    
+    init(model: MemorizeGame<String>) {
+        self.model = model
+    }
+}
