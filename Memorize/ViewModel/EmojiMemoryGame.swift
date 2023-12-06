ef896192a28c77274930ad88e881572eb6b8d443
ijkVector <johnsmith28122000@gmail.com>
2023-12-06 15:28:22 +0300

    Add: theme as var in ViewModel

--- Memorize/ViewModel/EmojiMemoryGame.swift
+++ Memorize/ViewModel/EmojiMemoryGame.swift
@@ -9,19 +9,31 @@ import SwiftUI
 
 
 class EmojiMemoryGame: ObservableObject {
-    private static let emojis = ["🦊", "🐿️", "🦔", "🐘", "🐄", "🦬", "🐝", "🦫", "🦑", "🐷", "🐓", "🦛","🐑"]
-    
-    private static func createMemoryGame() -> MemoryGame<String> {
-        return MemoryGame(numberOfPairsOfCards: 4) { pairIndex in
-            if emojis.indices.contains(pairIndex) {
-                return emojis[pairIndex]
+    private static func createMemoryGame(withTheme theme: Theme) -> MemoryGame<String> {
+        return MemoryGame(theme.numberOfPairsOfCards) { pairIndex in
+            if theme.emojis.indices.contains(pairIndex) {
+                return theme.emojis[pairIndex]
             } else {
                 return "⁉️"
             }
         }
     }
     
-    @Published private var game = createMemoryGame()
+    init(currentTheme: Theme) {
+        self.currentTheme = currentTheme
+        self.game = EmojiMemoryGame.createMemoryGame(withTheme: currentTheme)
+    }
+    
+    @Published private var game: MemoryGame<String>
+    @Published private(set) var currentTheme: Theme
+    private let themes = [
+        Theme(name: "Animals", emoijs: ["🦊", "🐿️", "🦔", "🐘", "🐄", "🦬", "🐝", "🦫", "🦑", "🐷", "🐓", "🦛","🐑"], numberOfPairs: 4, color: .orange),
+        Theme(name: "Plants", emoijs: ["🌹", "💐", "🌸", "🌺", "🍀", "🌷", "🌻", "🌿", "🌴", "🌳", "🌵", "🪴", "🌾"], numberOfPairs: 4, color: .green),
+        Theme(name: "Cars", emoijs: ["🏎️", "🚕", "🚙", "🚗", "🛺"], numberOfPairs: 4, color: .blue),
+        Theme(name: "Public transport", emoijs: ["✈️", "🚅", "🚇", "🚃", "🛳️"], numberOfPairs: 4, color: .yellow),
+        Theme(name: "Personal transport", emoijs: ["🚲", "🏍️", "🛴", "🚁", "⛵️"], numberOfPairs: 4, color: .purple),
+        Theme(name: "Halloween", emoijs:  ["🎃", "👻", "💀", "🕷️", "😈", "☠️", "🕸️"], numberOfPairs: 4, color: .red),
+    ]
      
     
     var cards: [MemoryGame<String>.Card] {
